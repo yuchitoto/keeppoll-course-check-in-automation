49bee02d64e3037d2a78bc823f9496e3f8309beb
Alan Tsui Ho Ming <33037271+heihaho@users.noreply.github.com>
2019-04-06 19:20:24 +0800

    Add Linux and Mac Functionality
    Not tested

---
+++ Mac.sh
@@ -0,0 +1,6 @@
+course=%1
+ses=%2
+link="https://poll.keep.edu.hk/a/%course%/%ses%"
+open -na "Google Chrome" $link || open -na safari $link 
+sleep  6s
+open -na "Google Chrome" $link || open -na safari $link 
